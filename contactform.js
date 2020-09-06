4e44f5c2781da3151e4fafcc8adafbd8bf3fe2ab
ivan_bal <semenov.siberian@gmail.com>
2020-09-06 15:49:26 +0700

    final web with form

---
+++ contactform.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+  $(".form-send").submit(function() {
+  var str = $(this).serialize();
+  
+  $.ajax({
+  type: "POST",
+  url: "contact.php",
+  data: str,
+  success: function(msg) {
+  if(msg == 'OK') {
+  result = '<p>Thank you!</p>';
+  $(".fields").hide();
+  } else {
+  result = msg;
+  }
+  $('.note').html(result);
+  }
+  });
+  return false;
+  });
+  });
